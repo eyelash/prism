dbb31bae81c851cd7b924bcc838435dba22f24ab
Elias Aebi <eyelash@users.noreply.github.com>
2024-03-16 17:02:27 +0100

    XML improvements

--- languages/xml.hpp
+++ languages/xml.hpp
@@ -6,6 +6,27 @@ constexpr auto xml_white_space = zero_or_more(choice(' ', '\t', '\n', '\r'));
 constexpr auto xml_name_start_char = choice(range('a', 'z'), range('A', 'Z'), ':', '_');
 constexpr auto xml_name_char = choice(xml_name_start_char, '-', '.', range('0', '9'));
 constexpr auto xml_name = sequence(xml_name_start_char, zero_or_more(xml_name_char));
+constexpr auto xml_escape = sequence(
+	'&',
+	choice(
+		sequence('#', one_or_more(range('0', '9'))),
+		sequence('#', 'x', one_or_more(hex_digit)),
+		xml_name
+	),
+	';'
+);
+constexpr auto xml_string = choice(
+	sequence(
+		'"',
+		repetition(choice(highlight(Style::ESCAPE, xml_escape), but(choice('"', '<')))),
+		optional('"')
+	),
+	sequence(
+		'\'',
+		repetition(choice(highlight(Style::ESCAPE, xml_escape), but(choice('\'', '<')))),
+		optional('\'')
+	)
+);
 
 constexpr auto xml_syntax = scope(
 	highlight(Style::COMMENT, xml_comment),
@@ -18,12 +39,13 @@ constexpr auto xml_syntax = scope(
 			xml_white_space,
 			'=',
 			xml_white_space,
-			highlight(Style::STRING, sequence('"', repetition(but('"')), '"')),
+			highlight(Style::STRING, xml_string),
 			xml_white_space
 		))),
-		choice('>', "/>")
+		optional(choice('>', "/>"))
 	)),
-	highlight(Style::KEYWORD, sequence("</", xml_name, xml_white_space, '>'))
+	highlight(Style::KEYWORD, sequence("</", xml_name, xml_white_space, optional('>'))),
+	highlight(Style::ESCAPE, xml_escape)
 );
 
 struct xml_file_name {
