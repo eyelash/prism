1f01ed4ec8f75c98ca0db56b31d02c65b058aa01
Elias Aebi <eyelash@users.noreply.github.com>
2024-03-28 11:30:47 +0100

    Haskell improvements

--- languages/haskell.hpp
+++ languages/haskell.hpp
@@ -1,4 +1,7 @@
+// https://www.haskell.org/onlinereport/haskell2010/haskellch2.html
+
 constexpr auto haskell_identifier_char = choice(range('a', 'z'), '_', range('A', 'Z'), range('0', '9'), '\'');
+constexpr auto haskell_operator_char = choice('!', '#', '$', '%', '&', '*', '+', '.', '/', '<', '=', '>', '?', '@', '\\', '^', '|', '-', '~', ':');
 
 struct haskell_block_comment {
 	static constexpr auto expression = sequence(
@@ -12,10 +15,10 @@ struct haskell_block_comment {
 };
 constexpr auto haskell_comment = choice(
 	reference<haskell_block_comment>(),
-	sequence("--", repetition(but('\n')))
+	sequence("--", not_(haskell_operator_char), repetition(but('\n')))
 );
 
-constexpr auto haskell_module_name = sequence(
+constexpr auto haskell_module = sequence(
 	highlight(Style::TYPE, sequence(range('A', 'Z'), zero_or_more(haskell_identifier_char))),
 	zero_or_more(sequence(
 		'.',
@@ -57,16 +60,26 @@ struct haskell_language {
 			zero_or_more(' '),
 			optional(highlight(Style::KEYWORD, c_keyword("qualified"))),
 			zero_or_more(' '),
-			optional(haskell_module_name),
+			optional(haskell_module),
 			zero_or_more(' '),
 			optional(choice(
 				highlight(Style::KEYWORD, c_keyword("hiding")),
 				highlight(Style::KEYWORD, c_keyword("as"))
 			))
 		),
-		// types
-		highlight(Style::TYPE, sequence(range('A', 'Z'), zero_or_more(haskell_identifier_char))),
-		// identifiers
+		// qualified operators and identifiers
+		sequence(
+			haskell_module,
+			optional(sequence(
+				'.',
+				choice(
+					highlight(Style::OPERATOR, one_or_more(haskell_operator_char)),
+					sequence(choice(range('a', 'z'), '_'), zero_or_more(haskell_identifier_char))
+				)
+			))
+		),
+		// unqualified operators and identifiers
+		highlight(Style::OPERATOR, one_or_more(haskell_operator_char)),
 		sequence(choice(range('a', 'z'), '_'), zero_or_more(haskell_identifier_char))
 	);
 };
